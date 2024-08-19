3727391d029dcecb22c8f6a67815b03f3633467a
hasacz257 <aa.adrian9191@gmail.com>
2024-08-19 14:07:11 +0100

    Create index.js
    index

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FontVoting from './FontVoting';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FontVoting />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
